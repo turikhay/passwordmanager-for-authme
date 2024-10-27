407486f983a81e00392500a26d48dbaa4bf07142
turikhay <git@turikhay.com>
2024-10-27 20:27:09 +0500

    DO NOT MERGE: only try 1.21.3

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -2,8 +2,8 @@ rootProject.name = "pwam"
 
 include(":common")
 include(":fabric")
-include(":fabric:1.18.2")
-include(":fabric:1.19.4")
-include(":fabric:1.20.4")
-include(":fabric:1.20.6")
+// include(":fabric:1.18.2")
+// include(":fabric:1.19.4")
+// include(":fabric:1.20.4")
+// include(":fabric:1.20.6")
 include(":fabric:1.21.3")
