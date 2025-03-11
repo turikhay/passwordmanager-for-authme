56623630c0c8a9f1dbf68d961e402b2c67033e84
turikhay <git@turikhay.com>
2025-03-11 14:15:01 +0500

    Downgrade Kotlin version to match embedded version

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -1,7 +1,7 @@
 plugins {
     `kotlin-dsl`
     `java-gradle-plugin`
-    kotlin("plugin.serialization") version "2.1.10"
+    kotlin("plugin.serialization") version "2.0.21"
 }
 
 java {
@@ -38,7 +38,7 @@ gradlePlugin {
 }
 
 dependencies {
-    implementation("org.jetbrains.kotlin:kotlin-gradle-plugin:2.1.10")
+    implementation("org.jetbrains.kotlin:kotlin-gradle-plugin:2.0.21")
     implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.6.3")
     implementation("net.fabricmc:fabric-loom:1.10-SNAPSHOT")
     implementation("com.modrinth.minotaur:Minotaur:2.+")
