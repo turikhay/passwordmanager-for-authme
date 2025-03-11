27f2f195960d6e227f6950f6e94f2068ec9605ce
turikhay <git@turikhay.com>
2025-03-11 14:12:07 +0500

    Upgrade Kotlin to 2.1.10

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -1,7 +1,7 @@
 plugins {
     `kotlin-dsl`
     `java-gradle-plugin`
-    kotlin("plugin.serialization") version "1.9.22"
+    kotlin("plugin.serialization") version "2.1.10"
 }
 
 java {
@@ -38,7 +38,7 @@ gradlePlugin {
 }
 
 dependencies {
-    implementation("org.jetbrains.kotlin:kotlin-gradle-plugin:1.9.23")
+    implementation("org.jetbrains.kotlin:kotlin-gradle-plugin:2.1.10")
     implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.6.3")
     implementation("net.fabricmc:fabric-loom:1.10-SNAPSHOT")
     implementation("com.modrinth.minotaur:Minotaur:2.+")
