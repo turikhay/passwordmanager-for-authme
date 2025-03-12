e7c68eb56604084d6665f92ca09df6ea285b3884
turikhay <git@turikhay.com>
2025-03-12 16:09:41 +0500

    fix ijDownloadSources

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,3 +1,13 @@
 plugins {
     base
 }
+
+// Idea (ijDownloadSources task) fails to download source jar for buildSrc dependencies
+// (e.g. Fabric API) because it is only using repositories from the root project (i.e. here)
+repositories {
+    mavenCentral()
+    maven {
+        name = "Fabric"
+        url = uri("https://maven.fabricmc.net/")
+    }
+}
